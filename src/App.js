1dd9d3f949c598e760b40353a33cd2ef1bdc56ad
rohan <panditrohan738@gmail.com>
2024-08-12 10:12:29 +0530

    work on responsive

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter ,Routes,Route } from "react-router-dom";
 import Login from "./components/Login";
+import Header from "./components/Header";
 
 function App() {
   return (
    <div>
     <BrowserRouter>
+    <Header/>
     <Routes>
       <Route path="/" element={<Login />} />
     </Routes>
